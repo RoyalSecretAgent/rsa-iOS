ab2f4cd01451ae33d8084bbf161147c670e611f2
sanghoyoon <sanghoway@gmail.com>
2017-04-17 03:16:32 +0900

    Networking 파일 생성

---
+++ RoyalSecretAgent/MappingError.swift
@@ -0,0 +1,9 @@
+//
+//  MappingError.swift
+//  RoyalSecretAgent
+//
+//  Created by 윤상호 on 2017. 4. 17..
+//  Copyright © 2017년 SanghoYoon. All rights reserved.
+//
+
+import Foundation
---
+++ RoyalSecretAgent/Response+ErrorInfo.swift
@@ -0,0 +1,9 @@
+//
+//  Response+ErrorInfo.swift
+//  RoyalSecretAgent
+//
+//  Created by 윤상호 on 2017. 4. 17..
+//  Copyright © 2017년 SanghoYoon. All rights reserved.
+//
+
+import Foundation
