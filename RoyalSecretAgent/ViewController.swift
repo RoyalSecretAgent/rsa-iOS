46073b690e99c5652f117ac8276fbdf0127195bb
sanghoyoon <sanghoway@gmail.com>
2017-04-23 16:03:04 +0900

    model과 viewcontrollers 파일들 생성

--- RoyalSecretAgent/ViewController.swift
+++ RoyalSecretAgent/ViewController.swift
@@ -6,28 +6,20 @@
 //  Copyright © 2017년 SanghoYoon. All rights reserved.
 //
 
-import Alamofire
+import UIKit
 
-extension DataResponse {
-    
-    /// `Result`의 값을 가지고 만든 새로운 `Result`를 사용하는 `DataResponse`를 반환합니다.
-    func flatMap<T>(_ transform: (Value) -> Result<T>) -> DataResponse<T> {
-        let result: Result<T>
-        switch self.result {
-        case .success(let value):
-            result = transform(value)
-        case .failure(let error):
-            result = .failure(error)
-        }
-        return DataResponse<T>(
-            request: self.request,
-            response: self.response,
-            data: self.data,
-            result: result,
-            timeline: self.timeline
-        )
-        
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
     }
-    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
 }
 
